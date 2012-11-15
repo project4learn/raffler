7d7211b8e8faf5af0cd006261df005bedf802860
freshlhy <freshlhy@gmail.com>
2012-11-15 17:10:52 +0800

    first commit

---
+++ app/assets/javascripts/raffler.js.coffee
@@ -0,0 +1,15 @@
+window.Raffler =
+  Models:
+    {}
+  Collections:
+    {}
+  Views:
+    {}
+  Routers:
+    {}
+  init: ->
+        new Raffler.Routers.Entries
+        Backbone.history.start()
+
+$(document).ready ->
+  Raffler.init()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '42e8d5f8089d09b0592f7f105b446ccab25b21a0451d9731b0aa5cea161b0ff0fce5fc39f21e2b2d67f03b5d762a395f8ff5461105586c50825657ad211182eb'
